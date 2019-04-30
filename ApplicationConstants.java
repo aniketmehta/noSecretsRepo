ca085932cc13f32988cc16309a07ccba78b36467
aniketmehta <aniketmehta@gmail.com>
2019-04-30 14:01:54 -0700

    Create ApplicationConstants.java

---
+++ ApplicationConstants.java
@@ -0,0 +1,38 @@
+public class ApplicationConstants {
+
+	private ApplicationConstants() {
+	}
+		 
+	 
+	 public static final String FILE_EXTENSION = "xml";
+	 public static final String CONTENT_TYPE = "application/octet-stream";
+	 public static final String DOT = ".";
+	 public static final String FILE_TYPE = "application/xml";	 
+	 
+	 public static final String CLOSING_BRACE = "]";
+	 public static final String PIPE = "|";
+	 public static final String NEW_LINE = "\n";
+	 public static final String FRONT_SLASH = "/";
+	 public static final String COMMA = ",";
+	 public static final String TILDE = "~";
+	 public static final String UNDERSCORE = "_";
+	 public static final String DATEFORMAT = "dd-MM-yyyy";
+	 public static final String DATEFORMAT_TIMESTAMP = "dd-MM-yyyy HH:mm:ss";
+	 public static final String TEMP_FILE_STORE = "src/main/resources/";
+	 public static final String SHA_TYPE = "SHA-256";
+	 public static final String PROPERTY_FILE = "config.properties";
+	 public static final String TEMP_FILENAME = "DependencySample.xml";
+	 public static final String TEMP_FILE_EXTENSION = ".xml";
+	 public static final String HTML_DOCTYPE = "<!DOCTYPE html>";
+	 public static final String AUTH_TYPE_BASIC = "basic";
+	 public static final String AUTH_TYPE_MTLS = "mtls";	 
+	 
+	 public static final String BLOB_FILE_NAME = "FILE.NAME";
+	 public static final String BLOB_FILE_SIZE = "FILE.SIZE";
+	 public static final String APPLICATION_NAME = "APP.NAME";
+	 
+	 public static final String UPLOAD_TYPE_FILE = "file";
+	 public static final String UPLOAD_TYPE_ACTIVEMQ = "activemq";
+
+	public static final String SERVICE_ADDR = "http://0.0.0.0:443";
+}
